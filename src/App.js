f3c51b6e3cfbc14684826ba5f1f6a4e5c41d3817
Stefano Franzin <stefano.franzin@gmail.com>
2020-09-04 13:02:42 +0200

    Basic layout

--- src/App.js
+++ src/App.js
@@ -4,7 +4,25 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      Owner Tracking SPA
+      <div id="layout">
+        <div id="header">HEader</div>
+        <div id="container">
+          <div id="tabs">
+            <div id="tab-motorcycles">
+              <div id="tab-motorcycles-map"></div>
+              <div id="tab-motorcycles-actions">
+                <div id="tab-motorcycles-actions-submit-trigger">+</div>
+                <div id="tab-motorcycles-actions-edit-trigger">Search/Edit - TODO</div>
+                <div id="tab-motorcycles-actions-help-trigger">?</div>
+              </div>
+              <div id="tab-motorcycles-submit-modal"></div>
+              <div id="tab-motorcycles-help-modal"></div>
+            </div>
+            <div id="tab-events">Events - TODO</div>
+          </div>
+        </div>
+        <div id="footer">Footer</div>
+      </div>
     </div>
   );
 }
