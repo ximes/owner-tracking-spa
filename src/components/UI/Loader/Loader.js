a2105f95769e0eb922974ae92f16af48db14b7fe
Stefano Franzin <stefano.franzin@gmail.com>
2020-09-27 18:31:29 +0200

    Other loaders

--- src/components/UI/Loader/Loader.js
+++ src/components/UI/Loader/Loader.js
@@ -1,10 +1,8 @@
 import React from 'react';
-import { Backdrop, CircularProgress} from "@material-ui/core";
+import { CircularProgress } from "@material-ui/core";
 
 export default function Loader(){
   return (
-  <Backdrop open>
     <CircularProgress color="secondary" />
-  </Backdrop>
   );
 }
